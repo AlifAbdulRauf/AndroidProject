6763c9b509e4668211df0a60df4a6881cf72eb34
afdalhalimamda <73575111+afdalhalimamda@users.noreply.github.com>
2022-09-19 22:58:28 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/androidproject/persetujuannimbinganTA.java
@@ -0,0 +1,13 @@
+package com.ptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class persetujuannimbinganTA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_persetujuannimbingan_ta);
+    }
+}
