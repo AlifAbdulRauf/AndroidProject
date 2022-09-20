36c50a1dccd605d1aa55511f4c2d1be4663494e6
faradilasuwandi <faradilasuwandi@gmail.com>
2022-09-20 15:19:20 +0700

    .

---
+++ app/src/main/java/com/example/androidproject/ProfilDanListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilDanListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_dan_list)
+    }
+}
