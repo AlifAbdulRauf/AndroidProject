1f0c0bb638182caf0a6f32b4c4cbcf64fc04fafa
Alif Abdul Rauf <2011522024_alif@student.unand.ac.id>
2022-12-07 20:55:54 +0700

    notifmanual

--- app/src/main/java/com/example/androidproject/activity/FormPembatalan.kt
+++ app/src/main/java/com/example/androidproject/activity/FormPembatalan.kt
@@ -1,26 +1,75 @@
 package com.example.androidproject.activity
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.androidproject.R
 import kotlinx.android.synthetic.main.form_pembatalan.*
 
 class FormPembatalan : AppCompatActivity() {
+
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "channelNAme"
+    val NOTIFICATION_ID = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.form_pembatalan)
+        createNotificationChannel()
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run{
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Notif Form Pembatalan")
+            .setContentText("Berhasil membatalkan mahasiswa ")
+            .setSmallIcon(androidx.core.R.drawable.notification_icon_background)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+
+
+
+
+
+
 
         btnBatal.setOnClickListener{
-            Intent(this, detailta::class.java).also {
-                startActivity(it)
-            }
+            notificationManager.notify(NOTIFICATION_ID,notification)
         }
 
         ibBackListLogBook.setOnClickListener{
             Intent(this, detailta::class.java).also {
                 finish()
             }
+        }
+    }
+
+    fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
         }
     }
 }
