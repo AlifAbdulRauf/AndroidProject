9cf25031b89b55a274736e46979221073130251f
faradilasuwandi <faradilasuwandi@gmail.com>
2022-12-08 08:19:05 +0700

    notif manual

--- app/src/main/java/com/example/androidproject/activity/UbahProfilActivity.kt
+++ app/src/main/java/com/example/androidproject/activity/UbahProfilActivity.kt
@@ -1,12 +1,78 @@
 package com.example.androidproject.activity
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.androidproject.R
+import kotlinx.android.synthetic.main.activity_ubah_pass.*
+import kotlinx.android.synthetic.main.activity_ubah_pass.btnEditPass
+import kotlinx.android.synthetic.main.activity_ubah_profil.*
+import kotlinx.android.synthetic.main.form_pembatalan.*
 
 class UbahProfilActivity : AppCompatActivity() {
+
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "channelNAme"
+    val NOTIFICATION_ID = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ubah_profil)
+        createNotificationChannel()
+
+        val intent = Intent(this,ProfilActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run{
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Notif Ubah Profil")
+            .setContentText("Berhasil mengubah profil")
+            .setSmallIcon(androidx.core.R.drawable.notification_icon_background)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+
+
+
+
+
+
+
+        btnEditProfil.setOnClickListener{
+            notificationManager.notify(NOTIFICATION_ID,notification)
+        }
+
+        ibBackUbahProfil.setOnClickListener{
+            Intent(this, detailta::class.java).also {
+                finish()
+            }
+        }
+    }
+
+    fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
     }
 }
